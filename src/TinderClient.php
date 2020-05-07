dac4428736dc0093235fdd3f154fdf9aa5aa8032
Whacko <not@gonna.tel>
2020-05-07 23:31:36 +0200

    Changed headers and made setAuthToken public for testing

--- src/TinderClient.php
+++ src/TinderClient.php
@@ -55,9 +55,12 @@ class TinderClient {
                 'base_url' => ['https://api.gotinder.com', []],
                 'defaults' => [
                     'headers' => [  'X-Auth-Token'  =>  '00000000-0000-4000-A000-000000000000',
-                                    'Content-Type'  =>  'application/json',
-                                    'User-Agent'    =>  'Tinder Android Version 2.2.3',
-                                    'Accept'        =>  'application/json']
+                                    'Accept'        =>  'application/json',
+                                    'Content-type: application/json',
+                                    'app_version: 3',
+                                    'platform: ios',
+                                    'User-Agent: Tinder/3.0.4 (iPhone; iOS 7.1; Scale/2.00)',
+                                    'os_version: 700001']
                 ]
             ]);
     }
@@ -82,7 +85,7 @@ class TinderClient {
      * @param $token
      * @throws Exception
      */
-    private function setTinderAuthenticationToken($token)
+    public function setTinderAuthenticationToken($token)
     {
         if(strlen($token) != 36)
             throw new Exception("The supplied Tinder Authentication Token is not in UUID4 format, it should be 36 characters long (including dashes).");
